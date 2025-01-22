5475db85472fec56b65c0788b3cd435e87397401
singunu <singunu17@gmail.com>
2025-01-22 16:37:54 +0900

    Fix: Mobile Scroll Problem 6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,8 +31,8 @@ export default function Home() {
   };
 
   return (
-    <div className="h-svh w-screen overflow-hidden relative">
-      <div className="absolute inset-0">
+    <div className="h-screen w-full flex flex-col overflow-hidden">
+      <div className="flex-1 relative">
         <AnimatePresence mode="wait">
           <motion.div
             key={currentSection}
@@ -41,7 +41,7 @@ export default function Home() {
             exit="exit"
             variants={variants}
             transition={{ duration: 0.5 }}
-            className="w-full h-full"
+            className="absolute inset-0"
           >
             {currentSection === 'about' && <AboutSection />}
             {currentSection === 'projects' && <ProjectSection />}
@@ -50,7 +50,7 @@ export default function Home() {
           </motion.div>
         </AnimatePresence>
       </div>
-      <div className="absolute inset-x-0 bottom-0 z-50">
+      <div className="sticky bottom-0 left-0 right-0 z-50">
         <Navbar onSectionChange={handleSectionChange} />
       </div>
     </div>
